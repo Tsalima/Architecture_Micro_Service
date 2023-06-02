ac29052bdb01ebc161feef7bb9bbb458da68245c
Tsalima <ssalimatahiri@gmail.com>
2023-06-02 20:23:04 +0100

    Architecture_micro_services

---
+++ geteway/src/main/java/org/miaad/geteway/GetewayApplication.java
@@ -0,0 +1,34 @@
+package org.miaad.geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetewayApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator routeLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE").id("r1"))
+				.route((r)->r.path("/products/**").uri("lb://PRODUCT-SERVICE").id("r2"))
+				.build();
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(
+			ReactiveDiscoveryClient rdc,
+			DiscoveryLocatorProperties properties
+	){
+		return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
+	}
+}
---
+++ geteway/src/test/java/org/miaad/geteway/GetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miaad.geteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
